84d1305c91cfb77f4aa5a91f5d5a31ce3ded2a18
Maxim <103184322+MaxAks@users.noreply.github.com>
2022-08-09 11:49:49 +0200

    MontenegroPro_bot_V1.0.7
    Key features:
    1) The bot is authorized by Api telegram
    2) The bot responds with a greeting with the name of the person who wrote to him
    3) The bot sends in private messages to the creator of the bot the ID of the person who wrote it
    4) Ability to send a city selection button with a welcome message
    5) Fixed repeated response to the "/start" request, now the bot sends an initial greeting message with a name and a city selection button
    6) Added bot response to pressing city buttons
    7) Selecting and sending a specialty in the city.
    8) Sending a specialist.
    9) Fixed a bug: the bot sends a second click of the submit button to specialists.
    10) Added foolproofing when sending unregistered commands.
    11) Fixed a bug where the bot hangs when sending specialists.
    12) Added the ability to change the city after sending a specialist.

--- city/City_Choice.rb
+++ city/City_Choice.rb
@@ -1,12 +1,20 @@
 class City_Choice
+  
   def city_choice
     Telegram::Bot::Client.run(TOKEN) do |bot|
       # ОТПРАВКА КНОПОК НАЧАЛО
         bot.listen do |message|
           case message
           when Telegram::Bot::Types::Message
-            kb = 
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Подгорица', callback_data: 'podgorica')
+            kb = [ 
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Подгорица', callback_data: 'podgorica'),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Будва', callback_data: 'budva'),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Котор', callback_data: 'kotor'),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Петровац', callback_data: 'petrovac'),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Бар', callback_data: 'bar'),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Герцег-Нови', callback_data: 'herceg_novi'),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Ульцинь', callback_data: 'ulcin')
+            ]
             markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
             bot.api.send_message(chat_id: message.chat.id, text: 'Выберите город', reply_markup: markup)
           break  
@@ -15,4 +23,5 @@ class City_Choice
       #ОТПРАВКА КНОПОК КОНЕЦ
     end
   end
+
 end
--- profession_send/Profession_Send.rb
+++ profession_send/Profession_Send.rb
@@ -1,16 +1,54 @@
 class Profession_Send
-  def profession_send
+
+#ВСЯ СТРАНА
+
+#КОНЕЦ ВСЕЙ СТРАНЫ
+
+#ПОДГОРИЦА
+
+  module Profession_Send_Podgorica
+    def profession_send_podgorica_photographer
     Telegram::Bot::Client.run(TOKEN) do |bot|
-    # ОТПРАВКА КНОПОК НАЧАЛО
         bot.listen do |message|
-        require './profession_base/photographer/Profession_photographer'
         pp1 = Profession_Photographer.new #Создаем объект класса Profession_Photographer
+        #Отправка профессионалов из выбранного города
         bot.api.send_message(
-                chat_id: message.from.id, 
-                text: "#{pp1.photographer_podgorica}") #Вызываем метод написания профессии и пишем его сразу в команде к API телеграма.
-          break
+        chat_id: message.from.id, 
+        text: "#{pp1.photographer_podgorica}\n #{pp1.photographer_all}") #Вызываем метод написания профессии и пишем его сразу в команде к API телеграма.
+        #Отправка профессионалов,работающих по всей стране
+        #bot.api.send_message(
+        #chat_id: message.from.id, 
+        #text: "#{pp1.photographer_all}") #Вызываем метод написания профессии и пишем его сразу в команде к API телеграма.
+        break
         end  
-      #ОТПРАВКА КНОПОК КОНЕЦ
       end
+    end
   end
+include Profession_Send_Podgorica
+#КОНЕЦ ПОДГОРИЦА
+
+  #БУДВА
+
+  #КОНЕЦ БУДВА
+
+  #КОТОР
+
+  #КОНЕЦ КОТОР
+  
+  #ПЕТРОВАЦ
+
+  #КОНЕЦ ПЕТРОВАЦ
+
+  #БАР
+
+  #КОНЕЦ БАР
+
+  #Герцег-Нови
+
+  #КОНЕЦ Герцег-Нови
+
+  #Ульцинь
+  
+  #КОНЕЦ Ульцинь 
+
 end
